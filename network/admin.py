8fcf986bf43d8bfdfef279a8e6c3e3928402db81
Uliana Semyonova <i@ulianasemyonova.ru>
2023-12-28 20:16:41 +0500

    Create admin for network app

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,31 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register, site, action
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from network.models import Company, Contact, Product
+
+
+site.register(Contact)
+site.register(Product)
+
+
+@register(Company)
+class CompanyAdmin(ModelAdmin):
+    list_display = ('name', 'company_type', 'link_to_supplier', )
+    list_display_links = ('name', 'link_to_supplier', )
+    list_filter = ('company_type', 'contacts__city')
+    search_fields = ('name', )
+    actions = ('clear_indebtedness', )
+
+    @action(description='Clear indebtedness')
+    def clear_indebtedness(self, request, queryset):
+        queryset.update(indebtedness=None)
+
+    def link_to_supplier(self, obj):
+        supplier = obj.supplier
+
+        if supplier:
+            url = reverse('admin:network_company_change', args=[supplier.id])
+            return format_html('<a href="{}">{}</a>', url, supplier)
+
+        return 'No supplier'
